2e0e33ae4093da2f8812a767521881d4c9581e4c
Sungpil An <ansungpil1@gmail.com>
2021-02-15 14:53:21 -0500

    finalize push before ReadMe

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,8 +4,7 @@ import './Header.css';
 function Header () {
     return (
         <div className="header">
-            <h1>Employee Directory</h1>
-            <p>Click on each heading to filter or Use the search bar to search specific employee by name</p>
+            <h1>Employee  Directory</h1>
         </div>
     )
 }
--- src/components/SearchName/index.js
+++ src/components/SearchName/index.js
@@ -8,16 +8,20 @@ const SearchName = () => {
     return (
         <div className="searchbox">
             <form className="form-inline">
-                <input
-                    className="form-control mr-sm-2"
-                    type="search"
-                    placeholder="Type Employee Name..."
-                    aria-label="Search"
-                    onChange={e => context.handleSearchChange(e)}
-                />
-                <button className="btn my-2 my-sm-0" type="submit">
-                    Search
-                </button>
+                <div className="input-group">
+                    <input
+                        className="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Type Employee Name..."
+                        aria-label="Search"
+                        onChange={e => context.handleSearchChange(e)}
+                    />
+                    <span className="input-group-btn">
+                    <button className="btn my-2 my-sm-0" type="submit">
+                        Search
+                    </button>
+                    </span>
+                </div>
             </form>
         </div>
     )
