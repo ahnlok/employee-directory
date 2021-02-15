77a8cfadb88f8a89171fe46df848c7a4d167b396
Sungpil An <ansungpil1@gmail.com>
2021-02-15 11:49:57 -0500

    added header.js

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Header() {
+    return (
+        <header className="center">
+            Employee Directory
+        </header>
+    )
+}
+
+export default Header;
